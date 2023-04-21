d84b63621c61d9e121dcb20cf1932fd9280f88f0
elcai01 <131410669+elcai01@users.noreply.github.com>
2023-04-21 00:45:27 -0500

    Add files via upload

---
+++ smtp.js
@@ -0,0 +1,16 @@
+﻿function sendEmail() {
+    Email.send({
+      To: "edwinpelaezcaicedo@gmail.com",
+      From: document.getElementById("email").value,
+      Subject: "Pregunta desde mi sitio web" + document.getElementById("name").value,
+      Body: "Hola,\n\nAlguien ha enviado una pregunta desde mi sitio web. La pregunta es:\n\n" + document.getElementById("question").value,
+    }).then(
+      message => alert("Correo enviado correctamente: " + message)
+    );
+  }
+  
+  document.getElementById("submit").addEventListener("click", function(event) {
+    event.preventDefault();
+    sendEmail();
+  });
+  
